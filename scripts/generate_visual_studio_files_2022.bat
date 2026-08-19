3fd7be0c059968c812438cb5c2a4dbe668e00840
Poivre31 <raphael.boisard@gmail.com>
2026-08-19 11:55:23 +0200

    Tweaks for windows, gcc and visual studio support

---
+++ scripts/generate_visual_studio_files_2022.bat
@@ -0,0 +1,4 @@
+cd ..
+mkdir visual_studio_2022
+cd visual_studio_2022
+cmake .. -G "Visual Studio 17 2022"
---
+++ scripts/generate_visual_studio_files_2026.bat
@@ -0,0 +1,4 @@
+cd ..
+mkdir visual_studio_2026
+cd visual_studio_2026
+cmake .. -G "Visual Studio 18 2026"
